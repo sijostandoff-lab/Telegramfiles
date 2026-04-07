afbabd06045fb5a255f8c99be51e33f742ce5535
sijostandoff-lab <sijo.standoff@gmail.com>
2026-04-07 17:11:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 import json
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.client.default import DefaultBotProperties
 
 TOKEN = "8693534383:AAEM2Ql3WfX4iHrBOTAmBSKWNSpViYXuH2I"
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+dp = Dispatcher()
 
 POSTS_FILE = "posts.json"
 
-# загрузка базы
+
 def load_posts():
     try:
         with open(POSTS_FILE, "r") as f:
@@ -17,58 +20,55 @@ def load_posts():
     except:
         return {}
 
-# сохранение базы
+
 def save_posts(data):
     with open(POSTS_FILE, "w") as f:
         json.dump(data, f)
 
+
 posts = load_posts()
 
 
-# /start
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    args = message.get_args()
+@dp.message(Command("start"))
+async def start(message: Message):
 
-    if args.startswith("post_"):
-        post_id = args.split("_")[1]
+    args = message.text.split(" ")
+
+    if len(args) > 1 and args[1].startswith("post_"):
+
+        post_id = args[1].split("_")[1]
 
         if post_id in posts:
-            file_id = posts[post_id]["file_id"]
-            file_type = posts[post_id]["type"]
-            text = posts[post_id].get("text")
 
-            if file_type == "photo":
-                await message.answer_photo(file_id, caption=text)
+            data = posts[post_id]
 
-            elif file_type == "video":
-                await message.answer_video(file_id, caption=text)
+            if data["type"] == "photo":
+                await message.answer_photo(data["file_id"], caption=data.get("text"))
 
-            elif file_type == "voice":
-                await message.answer_voice(file_id)
+            elif data["type"] == "video":
+                await message.answer_video(data["file_id"], caption=data.get("text"))
 
-            elif file_type == "video_note":
-                await message.answer_video_note(file_id)
+            elif data["type"] == "voice":
+                await message.answer_voice(data["file_id"])
 
-            elif file_type == "document":
-                await message.answer_document(file_id)
+            elif data["type"] == "video_note":
+                await message.answer_video_note(data["file_id"])
 
-            elif file_type == "text":
-                await message.answer(text)
+            elif data["type"] == "document":
+                await message.answer_document(data["file_id"])
 
-            else:
-                await message.answer("Неизвестный тип файла")
+            elif data["type"] == "text":
+                await message.answer(data["text"])
 
         else:
             await message.answer("Пост не найден")
 
 
-# команда получения ссылки
-@dp.message_handler(commands=["admingetlink"])
-async def get_link(message: types.Message):
+@dp.message(Command("admingetlink"))
+async def link(message: Message):
 
     if not message.reply_to_message:
-        await message.reply("Ответь на сообщение с медиа")
+        await message.reply("Ответь на пост")
         return
 
     msg = message.reply_to_message
@@ -77,62 +77,40 @@ async def get_link(message: types.Message):
     data = {}
 
     if msg.photo:
-        data = {
-            "type": "photo",
-            "file_id": msg.photo[-1].file_id,
-            "text": msg.caption
-        }
+        data = {"type": "photo", "file_id": msg.photo[-1].file_id, "text": msg.caption}
 
     elif msg.video:
-        data = {
-            "type": "video",
-            "file_id": msg.video.file_id,
-            "text": msg.caption
-        }
+        data = {"type": "video", "file_id": msg.video.file_id, "text": msg.caption}
 
     elif msg.voice:
-        data = {
-            "type": "voice",
-            "file_id": msg.voice.file_id
-        }
+        data = {"type": "voice", "file_id": msg.voice.file_id}
 
     elif msg.video_note:
-        data = {
-            "type": "video_note",
-            "file_id": msg.video_note.file_id
-        }
+        data = {"type": "video_note", "file_id": msg.video_note.file_id}
 
     elif msg.document:
-        data = {
-            "type": "document",
-            "file_id": msg.document.file_id
-        }
+        data = {"type": "document", "file_id": msg.document.file_id}
 
     elif msg.text:
-        data = {
-            "type": "text",
-            "text": msg.text
-        }
+        data = {"type": "text", "text": msg.text}
 
     else:
-        await message.reply("Тип медиа не поддерживается")
+        await message.reply("Тип не поддерживается")
         return
 
     posts[post_id] = data
     save_posts(posts)
 
-    link = f"https://t.me/{(await bot.get_me()).username}?start=post_{post_id}"
+    bot_info = await bot.get_me()
 
-    await message.reply(f"Ссылка:\n{link}")
+    link = f"https://t.me/{bot_info.username}?start=post_{post_id}"
+
+    await message.reply(link)
 
 
-# загрузка постов напрямую
-@dp.message_handler(content_types=types.ContentTypes.ANY)
-async def upload(message: types.Message):
-    await message.reply(
-        "Чтобы получить ссылку на пост, ответь на него командой:\n/admingetlink"
-    )
+async def main():
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    asyncio.run(main())
